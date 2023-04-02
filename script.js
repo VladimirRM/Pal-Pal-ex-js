0928fbec0171a86c0d421637f39d8893411ef567
Vladimir RM <shvdrummer@gmail.com>
2023-04-02 20:13:32 +0300

    Create template for this.

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
-const dog = {
-  name: "Sam",
-  gav: function () {
-    console.log(this.name);
-  },
-};
-const cat = {
-  name: "Mursic",
-  may: function () {
-    console.log(this.name);
-  },
-};
+// const dog = {
+//   name: "Sam",
+//   gav: function () {
+//     console.log(this.name);
+//   },
+// };
+// const cat = {
+//   name: "Mursic",
+//   may: function () {
+//     console.log(this.name);
+//   },
+// };
 
-const human = {
-  name: "John",
-};
-human.say = cat.may;
-human.say();
-dog.gav();
-cat.may();
+// const human = {
+//   name: "John",
+// };
+// human.say = cat.may;
+// human.say();
+// dog.gav();
+// cat.may();
+
+// const dog = {
+//   name: "Sam",
+// };
+// const cat = {
+//   name: "Mursic",
+// };
+
+// const say = function () {
+//   console.log(this.name);
+// };
+
+// dog.gav = say;
+// cat.may = say;
+// dog.gav();
+// cat.may();
+// say();
+
+// const cat = {
+//   name: "Bob",
+//   say() {
+//     console.log(this.name);
+//   },
+// };
+// cat.say();
---
+++ this.js
@@ -0,0 +1,46 @@
+// const dog = {
+//   name: "Sam",
+//   gav: function () {
+//     console.log(this.name);
+//   },
+// };
+// const cat = {
+//   name: "Mursic",
+//   may: function () {
+//     console.log(this.name);
+//   },
+// };
+
+// const human = {
+//   name: "John",
+// };
+// human.say = cat.may;
+// human.say();
+// dog.gav();
+// cat.may();
+
+// const dog = {
+//   name: "Sam",
+// };
+// const cat = {
+//   name: "Mursic",
+// };
+
+// const say = function () {
+//   console.log(this.name);
+// };
+
+// dog.gav = say;
+// cat.may = say;
+// dog.gav();
+// cat.may();
+// say();
+
+const cat = {
+    name: "Bob",
+    say() {
+      console.log(this.name);
+    },
+  };
+  cat.say();
+  
