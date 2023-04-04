652cd15abab810806faa239fad6f2826fe3d521d
Vladimir RM <shvdrummer@gmail.com>
2023-04-04 21:24:37 +0300

    Add & call method onj

--- script.js
+++ script.js
@@ -1,45 +1,9 @@
-// const dog = {
-//   name: "Sam",
-//   gav: function () {
-//     console.log(this.name);
-//   },
-// };
-// const cat = {
-//   name: "Mursic",
-//   may: function () {
-//     console.log(this.name);
+// const user = {
+//   name: "Damil",
+//   sayHello() {
+//     console.log(`hi my name is ${this.name} ,this`);
 //   },
 // };
 
-// const human = {
-//   name: "John",
-// };
-// human.say = cat.may;
-// human.say();
-// dog.gav();
-// cat.may();
-
-// const dog = {
-//   name: "Sam",
-// };
-// const cat = {
-//   name: "Mursic",
-// };
-
-// const say = function () {
-//   console.log(this.name);
-// };
-
-// dog.gav = say;
-// cat.may = say;
-// dog.gav();
-// cat.may();
-// say();
-
-// const cat = {
-//   name: "Bob",
-//   say() {
-//     console.log(this.name);
-//   },
-// };
-// cat.say();
+// user.sayHello();
+// const f = user.sayHello.bind(user);
